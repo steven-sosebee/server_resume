9debc58083701c5a458135e390a77b8cfc09a6f2
Steven Sosebee <swsosebee@gmail.com>
2023-11-20 14:04:25 -0500

    removed unnecessary model files

--- models/coverLetterModels/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once "createCoverLetterData.php";
-    require_once "getCoverLetterData.php";
-    require_once "deleteCoverLetterData.php";
-
-    class CoverLetter {
-        
-    }
-?>

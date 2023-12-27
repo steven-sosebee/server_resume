fc0d52ca444629b6535fe378b1a52f9a9b0fbb52
Steven Sosebee <swsosebee@gmail.com>
2023-12-27 11:02:14 -0500

    updated config paths

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__."/../config/config_defs.php";
-require_once __DIR__."/../env/config.php";
+require_once __DIR__."/config/config_defs.php";
+require_once __DIR__."/env/config.php";
 class Database {
     static protected $instance;
     // protected $connection;

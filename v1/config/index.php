6bafa1ed3bee31b474b65e41d0c40d04ab2c11eb
Steven Sosebee <swsosebee@gmail.com>
2024-01-16 11:16:08 -0500

    files to configures the specific server

---
+++ v1/config/index.php
@@ -0,0 +1,15 @@
+<?php
+    // $config="config";
+    // $privateConfig = parse_ini_file(PROJECT_ROOT_PATH.'/../private/config.ini');
+    // $publicConfig = parse_ini_file(__DIR__.'/config.ini');
+    // // ini_set("session.save_path", PROJECT_ROOT_PATH."/sessions");
+    
+    // // database configurations
+    // define("DB_HOST",$privateConfig['db_host']);
+    // define("DB_USERNAME",$privateConfig['db_userName']);
+    // define("DB_PASSWORD",$privateConfig['db_password']);
+    // define("DB_PORT",$privateConfig['port']);
+    // define("DB_RESUME", $publicConfig['DB_RESUME']);
+    // define("DB_ROOT", $publicConfig['DB_ROOT']);
+    // define("DB",DB_ROOT.DB_RESUME);
+?>
---
+++ v1/config/schema/index.php
@@ -0,0 +1,5 @@
+<?php
+    require_once "joins.php";
+    $joins = json_decode(file_get_contents(__DIR__."/joins.json"));
+
+?>
